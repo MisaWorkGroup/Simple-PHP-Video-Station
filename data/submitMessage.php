273e7cbe38eaa685f0d4627d7859b46eee29dd6d
HIMlaoS_Misa <62408957+MisaLiu@users.noreply.github.com>
2021-04-22 08:25:45 +0800

    Add files via upload

--- data/submitMessage.php
+++ data/submitMessage.php
@@ -1,85 +1,55 @@
 <?php
-
 define('_IN_STATION_', true);
 
 require_once('Config.php');
 require_once('preload.php');
 session_start();
 
-$value = array();
-
 $breakTime = $config['board']['breakTime'];
 $defaultState = $config['board']['defaultState'];
 
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-	if (isset($_SESSION['lastSubTime']) && (time() - $_SESSION['lastSubTime']) > $breakTime) {
-		if (isset($_SESSION['user'])) {
-			if ($_SESSION['user'] != ""){
-				if (strlen($_SESSION['user']) === $config['user']['hashLenth']) {
-					if ($_SESSION['user'] === $_POST['user']) {
-						
-						$value['id'] = null;
-						$value['username'] = htmlspecialchars(stripslashes(trim($_POST['username'])));
-						$value['email'] = htmlspecialchars(stripslashes(trim($_POST['email'])));
-						$value['website'] = htmlspecialchars(stripslashes(trim($_POST['website'])));
-						$value['timestamp'] = $db->now();
-						$value['message'] = $_POST['text'];
-						$value['state'] = $defaultState;
-
-						if (empty($value['username'])) {
-							echo('-20');
-							$_SESSION['lastSubTime'] = time();
-							die();
-						}
-						
-						if (empty($value['email'])) {
-							echo('-30');
-							$_SESSION['lastSubTime'] = time();
-							die();
-						}
-						
-						if (empty($value['message'])) {
-							echo('-50');
-							$_SESSION['lastSubTime'] = time();
-							die();
-						}
-						
-						if (new_strlen($value['message']) > $config['board']['msgLength']) {
-							echo('-60');
-							$_SESSION['lastSubTime'] = time();
-							die();
-						}
-						
-						$rowCount = $db->insert('messages', $value);
-
-						echo $rowCount;
-
-						$_SESSION['lastSubTime'] = time();
-						$_SESSION['user'] = rand_string($config['user']['hashLenth']);
-						
-					}else{
-						echo('-999');
-
-					}
-				}else{
-					echo('-999');
-
-				}
-			}else{
-				echo('-999');
-
-			}
-		}else{
-			echo('-999');
-
-		}
-	}else{
-		echo('-100');
-
-	}
-}
-
-
+$lastSubTime = $_SESSION['lastSubTime'];
 $_SESSION['lastSubTime'] = time();
 
+if ($_SERVER['REQUEST_METHOD'] != "POST")
+	die();
+
+if (empty($lastSubTime) || (time() - $lastSubTime) < $breakTime) 
+	die('-100');
+
+if (empty($_SESSION['user']))
+	die('-999');
+
+if (strlen($_SESSION['user']) !== $config['user']['hashLenth'])
+	die('-999');
+
+if ($_SESSION['user'] !== $_POST['user'])
+	die('-999');
+
+$value = array();
+
+$value['id'] = null;
+$value['username'] = htmlspecialchars(stripslashes(trim($_POST['username'])));
+$value['email'] = htmlspecialchars(stripslashes(trim($_POST['email'])));
+$value['website'] = htmlspecialchars(stripslashes(trim($_POST['website'])));
+$value['timestamp'] = $db->now();
+$value['message'] = $_POST['text'];
+$value['state'] = $defaultState;
+
+if (empty($value['username']))
+	die('-20');
+
+if (empty($value['email']))
+	die('-30');
+
+if (empty($value['message']))
+	die('-50');
+
+if (new_strlen($value['message']) > $config['board']['msgLength'])
+	die('-60');
+
+$rowCount = $db->insert('messages', $value);
+echo $rowCount;
+
+$_SESSION['user'] = rand_string($config['user']['hashLenth']);
 ?>
