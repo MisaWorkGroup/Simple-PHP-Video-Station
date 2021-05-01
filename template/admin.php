80bfb1cd08e7b37dd08762047a3c45feff816793
HIMlaoS_Misa <62408957+MisaLiu@users.noreply.github.com>
2021-05-01 19:59:59 +0800

    Update admin.php

--- template/admin.php
+++ template/admin.php
@@ -70,50 +70,52 @@
 	<? } ?>
 </div>
 
-<div class="mdui-card mdui-hoverable">
-	<div class="mdui-card-content">
-		<div class="mdui-typo" style="position:relative;top:-20px;">
-			<h1>公告板</h1>
-			<? if (!empty($admin['notice'])) {
-				 echo $global['site']['notice'];
-			} else { ?>
-				<center class="mdui-text-color-theme-disabled">你还没有设置站点公告</center>
-			<? } ?>
-		</div>
-	</div>
-	<? if ($admin['isAdmin']) { ?>
-		<div class="mdui-card-actions">
-			<button onclick="changeNotice()" class="mdui-btn mdui-ripple mdui-text-color-theme-accent mdui-float-right">修改公告</button>
-		</div>
-	<? } ?>
-</div>
-<br>
-
-<div class="mdui-card mdui-hoverable">
-	<div class="mdui-card-content">
-		<div class="mdui-typo" style="position:relative;top:-20px;">
-			<h1>视频列表</h1>
-		</div>
-		
-		<div class="mdui-container-fluid" style="padding:0 0 0 0;margin-top:5px;">
-			<div class="mdui-row" style="margin:0 0 0 0;">
-				<? if (isset($admin['videos'])) {
-						for ($i = 0; $i < count($admin['videos']); $i++) { ?>
-							<div class="mdui-col-xs-12 mdui-col-md-6" style="padding:0 0 0 0;">
-								<a href="javascript:editMedia(<? echo $admin['videos'][$i]['id']; ?>);" class="mdui-btn mdui-ripple mdui-center mdui-text-color-theme-accent mdui-text-truncate"><? echo $admin['videos'][$i]['name']; ?></a>
-							</div>
-					<? }
-					} else { ?>
-						<center><p>当前没找到视频～</p></center>
+<div id="media" class="mdui-p-a-2">
+	<div class="mdui-card mdui-hoverable">
+		<div class="mdui-card-content">
+			<div class="mdui-typo" style="position:relative;top:-20px;">
+				<h1>公告板</h1>
+				<? if (!empty($admin['notice'])) {
+					 echo $global['site']['notice'];
+				} else { ?>
+					<center class="mdui-text-color-theme-disabled">你还没有设置站点公告</center>
 				<? } ?>
 			</div>
 		</div>
+		<? if ($admin['isAdmin']) { ?>
+			<div class="mdui-card-actions">
+				<button onclick="changeNotice()" class="mdui-btn mdui-ripple mdui-text-color-theme-accent mdui-float-right">修改公告</button>
+			</div>
+		<? } ?>
 	</div>
-	<? if ($admin['isAdmin']) { ?>
-		<div class="mdui-card-actions">
-			<button onclick="addMedia()" class="mdui-btn mdui-ripple mdui-text-color-theme-accent mdui-float-right">添加视频</button>
+	<br>
+
+	<div class="mdui-card mdui-hoverable">
+		<div class="mdui-card-content">
+			<div class="mdui-typo" style="position:relative;top:-20px;">
+				<h1>视频列表</h1>
+			</div>
+			
+			<div class="mdui-container-fluid" style="padding:0 0 0 0;margin-top:5px;">
+				<div class="mdui-row" style="margin:0 0 0 0;">
+					<? if (isset($admin['videos'])) {
+							for ($i = 0; $i < count($admin['videos']); $i++) { ?>
+								<div class="mdui-col-xs-12 mdui-col-md-6" style="padding:0 0 0 0;">
+									<a href="javascript:editMedia(<? echo $admin['videos'][$i]['id']; ?>);" class="mdui-btn mdui-ripple mdui-center mdui-text-color-theme-accent mdui-text-truncate"><? echo $admin['videos'][$i]['name']; ?></a>
+								</div>
+						<? }
+						} else { ?>
+							<center><p>当前没找到视频～</p></center>
+					<? } ?>
+				</div>
+			</div>
 		</div>
-	<? } ?>
+		<? if ($admin['isAdmin']) { ?>
+			<div class="mdui-card-actions">
+				<button onclick="addMedia()" class="mdui-btn mdui-ripple mdui-text-color-theme-accent mdui-float-right">添加视频</button>
+			</div>
+		<? } ?>
+	</div>
 </div>
 
 </div>
