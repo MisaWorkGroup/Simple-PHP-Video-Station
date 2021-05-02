02ac47527692678afe21a7acded74fda56a33ea9
HIMlaoS_Misa <62408957+MisaLiu@users.noreply.github.com>
2021-05-02 20:37:54 +0800

    Update admin.php

--- data/admin.php
+++ data/admin.php
@@ -1,1 +1,125 @@
-<?php	define('_IN_STATION_', true);		require_once('Config.php');	require_once('preload.php');	session_start();		$breakTime = $config['board']['breakTime'];		$lastSubTime = $_SESSION['lastSubTime'];	$_SESSION['lastSubTime'] = time();		if ($_SERVER['REQUEST_METHOD'] != "POST")		die();		if (empty($lastSubTime) || (time() - $lastSubTime) < $breakTime) 		die('-100');		if (empty($_SESSION['user']))		die('-999');		if (strlen($_SESSION['user']) !== $config['user']['hashLenth'])		die('-999');		if ($_SESSION['user'] !== $_POST['userhash'])		die('-999');		if ($_POST['type'] == 'login') {		if ($_SESSION['isAdmin'])			die('-500');				if (empty($_POST['user']) || empty($_POST['password']))			die('-202');				if ($_POST['user'] == $config['admin']['user']) {			if ($config['admin']['ismd5'] && md5($_POST['password'], false) == md5($config['admin']['password'], false)) {				$_SESSION['isAdmin'] = true;				echo '0';							} elseif (!$config['admin']['ismd5'] && $_POST['password'] == $config['admin']['password']) {				$_SESSION['isAdmin'] = true;				echo '0';							} else {				die('-50');						}		} else {			die('-50');				}			} elseif ($_POST['type'] == 'global') {		if (!$_SESSION['isAdmin'])			die('-500');				if (empty($_POST['data']))			die('-999');				$obj = json_decode($_POST['data'], true);		$return = 0;				if(count($obj) != 7)			die('-200');				$db->where('config', 'sitename');		if (!$db->update('config', Array('value' => $obj['sitename'])))			$return -= 10;				$db->where('config', 'sitekeywords');		if (!$db->update('config', Array('value' => $obj['sitekeywords'])))			$return -= 20;				$db->where('config', 'sitedescription');		if (!$db->update('config', Array('value' => $obj['sitedescription'])))			$return -= 30;				$db->where('config', 'siteauthor');		if (!$db->update('config', Array('value' => $obj['siteauthor'])))			$return -= 40;				$db->where('config', 'sitenotice');		if (!$db->update('config', Array('value' => $obj['sitenotice'])))			$return -= 50;				$db->where('config', 'siteabout');		if (!$db->update('config', Array('value' => $obj['siteabout'])))			$return -= 60;				$db->where('config', 'sitefooter');		if (!$db->update('config', Array('value' => $obj['sitefooter'])))			$return -= 70;				echo $return;			} elseif ($_POST['type'] == 'media') {		if (!$_SESSION['isAdmin'])			die('-500');				if (empty($_POST['id']) || empty($_POST['name']) || empty($_POST['url']) || empty($_POST['do']))			die('-999');				if ($_POST['do'] == 1) {			$value = Array(				'name' => htmlspecialchars(stripslashes(trim($_POST['name']))),				'url' => htmlspecialchars(stripslashes(trim($_POST['url'])))			);			if ($db->insert('data', $value))				echo 'success';			else				echo 'failed';					} elseif ($_POST['do'] == 2) {			$db->where('name', htmlspecialchars(stripslashes(trim($_POST['name']))));			if ($db->update('data', Array('url' => htmlspecialchars(stripslashes(trim($_POST['url']))))))				echo 'success';			else				echo 'failed';					}			} else {		die();		 	}?>
+<?php
+	define('_IN_STATION_', true);
+	
+	require_once('Config.php');
+	require_once('preload.php');
+	session_start();
+	
+	$breakTime = $config['board']['breakTime'];
+	
+	$lastSubTime = $_SESSION['lastSubTime'];
+	$_SESSION['lastSubTime'] = time();
+	
+	if ($_SERVER['REQUEST_METHOD'] != "POST")
+		die();
+	
+	if (empty($lastSubTime) || (time() - $lastSubTime) < $breakTime) 
+		die('-100');
+	
+	if (empty($_SESSION['user']))
+		die('-999');
+	
+	if (strlen($_SESSION['user']) !== $config['user']['hashLenth'])
+		die('-999');
+	
+	if ($_SESSION['user'] !== $_POST['userhash'])
+		die('-999');
+	
+	if ($_POST['type'] == 'login') {
+		if ($_SESSION['isAdmin'])
+			die('-500');
+		
+		if (empty($_POST['user']) || empty($_POST['password']))
+			die('-202');
+		
+		if ($_POST['user'] == $config['admin']['user']) {
+			if ($config['admin']['ismd5'] && md5($_POST['password'], false) == md5($config['admin']['password'], false)) {
+				$_SESSION['isAdmin'] = true;
+				echo '0';
+				
+			} elseif (!$config['admin']['ismd5'] && $_POST['password'] == $config['admin']['password']) {
+				$_SESSION['isAdmin'] = true;
+				echo '0';
+				
+			} else {
+				die('-50');
+			
+			}
+		} else {
+			die('-50');
+		
+		}
+		
+	} elseif ($_POST['type'] == 'global') {
+		if (!$_SESSION['isAdmin'])
+			die('-500');
+		
+		if (empty($_POST['data']))
+			die('-999');
+		
+		$obj = json_decode($_POST['data'], true);
+		$return = 0;
+		
+		if(count($obj) != 7)
+			die('-200');
+		
+		$db->where('config', 'sitename');
+		if (!$db->update('config', Array('value' => $obj['sitename'])))
+			$return -= 10;
+		
+		$db->where('config', 'sitekeywords');
+		if (!$db->update('config', Array('value' => $obj['sitekeywords'])))
+			$return -= 20;
+		
+		$db->where('config', 'sitedescription');
+		if (!$db->update('config', Array('value' => $obj['sitedescription'])))
+			$return -= 30;
+		
+		$db->where('config', 'siteauthor');
+		if (!$db->update('config', Array('value' => $obj['siteauthor'])))
+			$return -= 40;
+		
+		$db->where('config', 'sitenotice');
+		if (!$db->update('config', Array('value' => $obj['sitenotice'])))
+			$return -= 50;
+		
+		$db->where('config', 'siteabout');
+		if (!$db->update('config', Array('value' => $obj['siteabout'])))
+			$return -= 60;
+		
+		$db->where('config', 'sitefooter');
+		if (!$db->update('config', Array('value' => $obj['sitefooter'])))
+			$return -= 70;
+		
+		echo $return;
+		
+	} elseif ($_POST['type'] == 'media') {
+		if (!$_SESSION['isAdmin'])
+			die('-500');
+		
+		if (empty($_POST['id']) || empty($_POST['name']) || empty($_POST['url']) || empty($_POST['do']))
+			die('-999');
+		
+		if ($_POST['do'] == 1) {
+			$value = Array(
+				'name' => htmlspecialchars(stripslashes(trim($_POST['name']))),
+				'url' => htmlspecialchars(stripslashes(trim($_POST['url'])))
+			);
+			if ($db->insert('data', $value))
+				echo 'success';
+			else
+				echo 'failed';
+			
+		} elseif ($_POST['do'] == 2) {
+			$db->where('name', htmlspecialchars(stripslashes(trim($_POST['name']))));
+			if ($db->update('data', Array('url' => htmlspecialchars(stripslashes(trim($_POST['url']))))))
+				echo 'success';
+			else
+				echo 'failed';
+			
+		}
+		
+	} else {
+		die();
+	}
+?>
